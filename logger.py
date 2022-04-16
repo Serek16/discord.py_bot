ac40be0b0c15ef7eb230bd819998d0455fdd0097
Sergiusz <xbxbcvbxc@gmail.com>
2022-04-16 08:51:05 +0000

    Loggers can write to specifed files now

--- logger.py
+++ logger.py
@@ -2,23 +2,23 @@ import logging
 import sys
 
 FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
-LOG_FILE = "./logs/discord_bot.log"
+LOG_DIR = "./logs/"
 
 def get_console_handler():
    console_handler = logging.StreamHandler(sys.stdout)
    console_handler.setFormatter(FORMATTER)
    return console_handler
 
-def get_file_handler():
-   file_handler = logging.FileHandler(LOG_FILE)
+def get_file_handler(file_name):
+   file_handler = logging.FileHandler(LOG_DIR + file_name)
    file_handler.setFormatter(FORMATTER)
    return file_handler
 
-def get_logger(logger_name):
+def get_logger(logger_name, file_name = "discord_bot.log"):
    logger = logging.getLogger(logger_name)
    logger.setLevel(logging.DEBUG)
    logger.addHandler(get_console_handler())
-   logger.addHandler(get_file_handler())
+   logger.addHandler(get_file_handler(file_name))
    # with this pattern, it's rarely necessary to propagate the error up to parent
    logger.propagate = False
    return logger
