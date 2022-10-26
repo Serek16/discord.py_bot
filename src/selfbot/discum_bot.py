417713430fa03e66a8f0c3fd3b31922d9a119db1
Sergiusz <xbxbcvbxc@gmail.com>
2022-10-26 20:47:58 +0200

    Delete unused method from discum_bot.py

--- src/selfbot/discum_bot.py
+++ src/selfbot/discum_bot.py
@@ -18,18 +18,6 @@ class DiscumBot:
     def __run_client_in_thread(self):
         self.client.gateway.run()
 
-    def __triggerSlashCommand(self, resp, guild_id: int, channel_id: int, bot_id: int, command_name: str,
-                              command_args: dict):
-        if resp.event.ready_supplemental:
-            self.client.gateway.request.searchSlashCommands(str(guild_id), limit=10, query=command_name)
-        if resp.event.guild_application_commands_updated:
-            self.client.gateway.removeCommand(self.__triggerSlashCommand)
-            slash_cmds = resp.parsed.auto()['application_commands']
-            s = SlashCommander(slash_cmds, application_id=str(bot_id))
-            data = s.get([command_name], inputs=command_args)
-            self.client.triggerSlashCommand(str(bot_id), channelID=str(channel_id), guildID=str(guild_id), data=data,
-                                            sessionID=self.client.gateway.session_id)
-
     def sendSlashCommand(self, slash_command: SlashCommand):
         guild_id = slash_command.guild_id
         channel_id = slash_command.channel_id
