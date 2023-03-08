12a7e3d7e383c00bd664d2726faa567968677108
Sergiusz <xbxbcvbxc@gmail.com>
2023-03-08 14:59:32 +0100

    Modify class setters

--- src/model/member.py
+++ src/model/member.py
@@ -2,9 +2,16 @@ from datetime import datetime
 
 
 class Member:
-    def __init__(self, member_id: int, username: str, level: int = 0, first_join: datetime = datetime.now(),
-                 last_join: datetime = datetime.now(), last_update: datetime = datetime.now(),
-                 is_banned: bool = False, member_left: bool = False):
+    def __init__(self,
+                 member_id: int,
+                 username: str,
+                 level: int = 0,
+                 first_join: datetime = datetime.now(),
+                 last_join: datetime = datetime.now(),
+                 last_update: datetime = datetime.now(),
+                 is_banned: bool = False,
+                 member_left: bool = False
+                 ):
         self._member_id = member_id
         self._username = username
         self._level = level
@@ -24,7 +31,7 @@ class Member:
 
     @username.setter
     def username(self, username):
-        if username is not self._username:
+        if username != self._username:
             self.last_update = datetime.now()
         self._username = username
 
@@ -34,7 +41,7 @@ class Member:
 
     @level.setter
     def level(self, level):
-        if level is not self._level:
+        if level != self._level:
             self.last_update = datetime.now()
         self._level = level
 
@@ -44,6 +51,8 @@ class Member:
 
     @first_join.setter
     def first_join(self, first_join):
+        if first_join != self.first_join:
+            self.last_update = datetime.now()
         self._first_join = first_join
 
     @property
@@ -52,7 +61,7 @@ class Member:
 
     @last_join.setter
     def last_join(self, last_join):
-        if last_join is not self._last_join:
+        if last_join != self._last_join:
             self.last_update = datetime.now()
         self._last_join = last_join
 
@@ -62,7 +71,7 @@ class Member:
 
     @is_banned.setter
     def is_banned(self, is_banned):
-        if is_banned is not self.is_banned:
+        if is_banned != self.is_banned:
             self.last_update = datetime.now()
         self._is_banned = is_banned
 
@@ -72,6 +81,6 @@ class Member:
 
     @member_left.setter
     def member_left(self, member_left):
-        if member_left is not self._member_left:
+        if member_left != self._member_left:
             self.last_update = datetime.now()
         self._member_left = member_left
